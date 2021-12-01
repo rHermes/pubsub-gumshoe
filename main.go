5f676a7cadbc08acdb739dd763784b833102a601
Teodor Spæren <teodor@sparen.no>
2021-12-01 09:37:30 +0100

    Add license

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2021 Teodor Spæren
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package main
 
 import (
@@ -69,11 +82,7 @@ func MonitorTopicCmd(c *cli.Context) error {
 	log.Printf("Listening!")
 	err = sub.Receive(cc, func(ctx context.Context, msg *pubsub.Message) {
 		defer msg.Nack()
-
-		fmt.Printf("Message from: %s\n", msg.PublishTime.String())
-		fmt.Printf("Message ID: %s\n", msg.ID)
-		fmt.Printf("Data:\n%s\n", msg.Data)
-
+		fmt.Println(string(msg.Data))
 		msg.Ack()
 	})
 	if err != nil {
